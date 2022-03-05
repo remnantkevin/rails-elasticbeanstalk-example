10c1c3de7cbab4368ad94d458c87e4fa71f98ff9
Kevin Elliott <skiimilk@gmail.com>
2022-03-05 16:09:07 +0800

    Test echo-ing from hook

--- .platform/hooks/prebuild/00_upgrade_to_postgresql13.sh
+++ .platform/hooks/prebuild/00_upgrade_to_postgresql13.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 amazon-linux-extras enable postgresql13
 yum clean metadata
 yum install postgresql
 yum list installed postgresql
+echo "from the prebuild hook"
 psql --version
