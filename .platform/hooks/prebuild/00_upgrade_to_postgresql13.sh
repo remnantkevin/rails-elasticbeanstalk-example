e311991e7b197fcbb932351863306cbb34f17eae
Kevin Elliott <skiimilk@gmail.com>
2022-03-07 07:58:48 +0800

    Add comment explaining command line option in prebuild hook

--- .platform/hooks/prebuild/00_upgrade_to_postgresql13.sh
+++ .platform/hooks/prebuild/00_upgrade_to_postgresql13.sh
@@ -8,6 +8,7 @@ amazon-linux-extras enable postgresql13 > /dev/null 2>&1
 echo "Running command yum clean metadata"
 yum clean metadata
 
+# --assumeyes: Automatically answer yes for all questions.
 echo "Running command yum install postgresql --assumeyes"
 yum install postgresql --assumeyes
 
