ddd9eaed3c0a6f5edd7b673bcc13d9223d70a4b9
Kevin Elliott <skiimilk@gmail.com>
2022-03-08 15:18:14 +0800

    Add hook to rename and use the correct Procfile for the relevant environment

---
+++ .platform/hooks/prebuild/02_rename_procfile.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "------- START: 02_use_procfile.sh -------"
+
+IS_WORKER=$(/opt/elasticbeanstalk/bin/get-config environment -k IS_WORKER)
+
+echo "IS_WORKER"
+echo $IS_WORKER
+
+if [ $IS_WORKER ]; then
+  mv Procfile.worker Procfile
+else
+  mv Procfile.web Profile
+fi
+
+echo "------- END: 02_use_procfile.sh -------"
