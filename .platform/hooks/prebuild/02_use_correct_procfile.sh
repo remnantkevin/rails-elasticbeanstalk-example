ce48906fa3e94139944fe1db6c5af5b1dece19c2
Kevin Elliott <skiimilk@gmail.com>
2022-03-09 14:45:20 +0800

    Fix condition check in prebuild hook

--- .platform/hooks/prebuild/02_use_correct_procfile.sh
+++ .platform/hooks/prebuild/02_use_correct_procfile.sh
@@ -7,7 +7,7 @@ IS_WORKER=$(/opt/elasticbeanstalk/bin/get-config environment -k IS_WORKER)
 echo "IS_WORKER"
 echo $IS_WORKER
 
-if [ $IS_WORKER ]; then
+if [ $IS_WORKER == true ]; then
   mv Procfile.worker Procfile
 else
   mv Procfile.web Procfile
